ff0f11c7df0bb14b4392c8bb1c4954761b59f234
juliialzate <julialzatesaa@gmail.com>
2024-02-07 07:22:53 -0500

    Actualizaciónn

---
+++ repetidor.py
@@ -0,0 +1,8 @@
+cadena = input ("Ingrese una cadena de texto")
+r = int (input ("Ingrese la cantidad de repeticiones")
+    print (cadena * r)
+
+for i in range (r):
+    print (cad) 
+         
+        
